b7ef780af464b8daf371dd469634aad24f01b386
mexanik619 <itzmexanik.dnd@gmail.com>
2026-01-17 01:18:05 +0530

    Research Purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <Eigen/Dense>
+using namespace Eigen;
+using namespace std;
 
 int main(){
-    
+    MatrixXd b(2,2);
+    b << 1,2,3,4;
+
+    int h = b.determinant();
+    cout << h << endl;
 }
